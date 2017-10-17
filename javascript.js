dbc87d8b910e657ce7cfd776d2240cc9bd2c0485
Emily Blackman <emilyblackman2019@u.northwestern.edu>
2017-10-17 13:41:34 +0200

    testing navbar and jquery

---
+++ javascript.js
@@ -0,0 +1,19 @@
+//window.alert("it's working!");
+
+var main = function() {
+  $(".topNav").click(function(){
+    $(".test"){
+      display:"block"
+    }
+  });
+
+  
+  /*if (x.className === "topnav") {
+    x.className += " responsive";
+  }
+  else {
+    x.className = "topnav";
+  } */
+};
+
+$(document).ready(main);

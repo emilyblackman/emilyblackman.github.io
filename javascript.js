0929715f641d02a696d7f26da342523648fd6e33
Emily Blackman <emilyblackman2019@u.northwestern.edu>
2017-10-17 14:11:18 +0200

    navbar is functional

--- javascript.js
+++ javascript.js
@@ -1,19 +1,27 @@
-//window.alert("it's working!");
 
 var main = function() {
-  $('.topNav').click(function(){
-    $('.test').animate({
-      display:'block'
-    }, 1)
-  });
-
+  // initially show only the about page
+  $('.projects').hide();
+  $('.contact').hide();
   
-  /*if (x.className === "topnav") {
-    x.className += " responsive";
-  }
-  else {
-    x.className = "topnav";
-  } */
+  // when any link is clicked, show that page and hide the others
+  $('#projects').click(function(){
+    $('.projects').show();
+    $('.about').hide();
+    $('.contact').hide();
+  })
+
+  $('#contact').click(function(){
+    $('.contact').show();
+    $('.about').hide();
+    $('.projects').hide();
+  })
+
+    $('#about').click(function(){
+    $('.about').show();
+    $('.projects').hide();
+    $('.contact').hide();
+  })
 };
 
 $(document).ready(main);
